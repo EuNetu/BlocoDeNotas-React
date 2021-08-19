c77a3280eb57643e0c502d39bcf1b0cdfb083adb
Neto25 <euneto2501@gmail.com>
2021-08-18 23:46:05 -0300

    criacao dos componentes de listagem, formulario e notas // criado css de notas

---
+++ ceep/src/components/ListaDeNotas.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import CardNota from './CardNota/CardNota';
+
+ export default class ListaDeNotas extends Component {
+  render(){
+    return(    
+      <ul>
+        {Array.of("Trabalho", "Trabalho", "Estudos").map((categoria, index) =>{
+          return (
+            <li key={index}>
+              <CardNota/>
+            </li>
+          );
+        })}
+
+      </ul>
+    );
+  }
+}
